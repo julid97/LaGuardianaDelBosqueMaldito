25abfb79a719c26aa2fdfe561573e3cb8832f324
julid97 <julid_2010@hotmail.com.ar>
2025-09-20 05:26:30 -0300

    UI de vida del Player

--- Assets/Scripts/EnemyCombat.cs
+++ Assets/Scripts/EnemyCombat.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
 public class EnemyCombat : MonoBehaviour
-{   
-    public int damage;
+{  
+   public int damage;
     private void OnCollisionEnter2D(Collision2D collision)
     {   
         if (collision.gameObject.CompareTag("Player"))
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,17 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public TextMeshProUGUI healthValue;
+
     public int lives = 5;
 
     public void ChangeHealth(int Damage)
     {  
         lives -= Damage;
+        healthValue.text = lives.ToString();
         
         if (lives <= 0)
         {

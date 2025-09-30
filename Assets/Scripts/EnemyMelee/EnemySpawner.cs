ed7b33c6fbac31104e5962c986a6ad85d2d3a21c
julid97 <julid_2010@hotmail.com.ar>
2025-09-30 04:26:39 -0300

    Cree el prefab de enemigo y un script EnemySpawner

---
+++ Assets/Scripts/EnemyMelee/EnemySpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    public  Vector2[] spawnPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < spawnPos.Length; i++)
+        { 
+            Vector3 pos = new Vector3(spawnPos[i].x, spawnPos[i].y, 0f);
+            Instantiate(enemyPrefab, pos, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

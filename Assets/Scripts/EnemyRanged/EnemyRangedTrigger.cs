9cf1b53a8a8bc2908d5e8aa2997d020fa28d9959
julid97 <julid_2010@hotmail.com.ar>
2025-10-02 04:50:34 -0300

    Algunas implementaciones del level design con tilemaps

--- Assets/Scripts/EnemyRanged/EnemyRangedTrigger.cs
+++ Assets/Scripts/EnemyRanged/EnemyRangedTrigger.cs
@@ -8,8 +8,6 @@ public class EnemyRangedTrigger : MonoBehaviour
     {
         enemy = GetComponentInParent<EnemyRangedCombat>();
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))

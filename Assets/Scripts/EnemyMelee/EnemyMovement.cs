16f8b1f8d52c30a864ce5586a0d6d425c44c831b
karina-montenegro <153586808+karina-montenegro@users.noreply.github.com>
2025-10-17 23:53:57 -0300

    walk-enemy1

--- Assets/Scripts/EnemyMelee/EnemyMovement.cs
+++ Assets/Scripts/EnemyMelee/EnemyMovement.cs
@@ -8,12 +8,17 @@ public class EnemyMovement : MonoBehaviour
 
     private Rigidbody2D _rb;
 
-    private Transform _player;   
-      
+    private Transform _player;
+    //test animacion 
+    private Animator _animator;
+    private Vector2 _lastDirection;
+
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        //test animacion
+        _animator = GetComponent<Animator>();
     }
     void Update()
     {
@@ -22,6 +27,24 @@ public class EnemyMovement : MonoBehaviour
             Vector2 direction = (_player.position - transform.position).normalized;
 
             _rb.linearVelocity = direction * speed;
+            //test animacion - Enviar direccion al animator
+            _animator.SetFloat("MovementX", direction.x);
+            _animator.SetFloat("MovementY", direction.y);
+            _animator.SetFloat("Movement", direction.magnitude);
+            //personaje quedara en posicion que deja de caminar
+            if (direction.magnitude > 0.01f)
+            {
+                _lastDirection = direction;
+            }
+            else
+            {//detener movimiento
+                _rb.linearVelocity = Vector2.zero;
+                //mantener ultima direccion mirada
+                _animator.SetFloat("MovementX", _lastDirection.x);
+                _animator.SetFloat("MovementY", _lastDirection.y);
+                _animator.SetFloat("Movement", 0f);
+
+            }
         }
     }
 

1638a5d8ea441c83756b727c929c127bcae97735
julid97 <julid_2010@hotmail.com.ar>
2025-10-31 22:11:46 -0300

    Personaje Principal: animación de caminata

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -5,19 +5,41 @@ public class PlayerMovements : MonoBehaviour
 {   
     private Rigidbody2D _rigidbody;
 
+    private Animator _animator;
+
+    private Vector2 _lastDirection;
+
+    private Vector2 _moveInput;
+
     [SerializeField] private float _speed;
     private void Start()
     {
        _rigidbody = GetComponent<Rigidbody2D>();
+       _animator = GetComponent<Animator>();
     }
 
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity = _moveInput * _speed;
+
+        if(_moveInput != Vector2.zero)
+        {
+            _lastDirection = _moveInput;
+
+            _animator.SetFloat("MovementX", _moveInput.x);
+            _animator.SetFloat("MovementY", _moveInput.y);
+        }
+        else
+        {
+            _animator.SetFloat("MovementX", _lastDirection.x);
+            _animator.SetFloat("MovementY", _lastDirection.y);
+        }
+
+        _animator.SetBool("IsMoving", _moveInput != Vector2.zero);
+    }
     private void OnMove(InputValue inputValue) {
         
-        var moveInput = inputValue.Get<Vector2>();
+        _moveInput = inputValue.Get<Vector2>();
 
-        _rigidbody.linearVelocity = moveInput * _speed;
-
-
-        
     }
 }

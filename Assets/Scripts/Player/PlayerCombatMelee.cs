1a61ca643a895d637c031a3c7cfc11910a64c494
julid97 <julid_2010@hotmail.com.ar>
2025-11-01 06:07:29 -0300

    Personaje principal animaciones: Ataque con espada

--- Assets/Scripts/Player/PlayerCombatMelee.cs
+++ Assets/Scripts/Player/PlayerCombatMelee.cs
@@ -1,16 +1,67 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCombatMelee : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator _animator;
+    public Transform swordHit;
+    public float swordRange = 0.7f;
+    public LayerMask enemyLayer;
+    public int damage = 1;
+
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnAttackMelee(InputValue value)
     {
-        
+        _animator.SetTrigger("AttackMelee");
+        StartCoroutine(AttackRoutine());
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        // Espera hasta el frame donde impacta la espada
+        yield return new WaitForSeconds(0.33f);
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(swordHit.position, swordRange, enemyLayer);
+
+        foreach (var enemy in enemies)
+        {
+            // Intenta obtener EnemyHealth
+            var enemyHealth = enemy.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.Damager(damage);
+                continue;
+            }
+
+            // Intenta obtener EnemyRangedHealth
+            var enemyRangedHealth = enemy.GetComponent<EnemyRangedHealth>();
+            if (enemyRangedHealth != null)
+            {
+                enemyRangedHealth.Damager(damage);
+                continue;
+            }
+
+            //Intenta obtener BossHealth
+            var bossHealth = enemy.GetComponent<BossHealth>();
+            if (bossHealth != null)
+            {
+                bossHealth.Damager(damage);
+                continue;
+            }
+        }
+    }
+
+    //Dibuja el rango del ataque en la escena
+    private void OnDrawGizmosSelected()
+    {
+        if (swordHit == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(swordHit.position, swordRange);
     }
 }
+

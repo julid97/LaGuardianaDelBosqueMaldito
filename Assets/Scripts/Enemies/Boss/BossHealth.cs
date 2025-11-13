486f44419f000472ff6246ee32f6a414de6a5c06
julid97 <julid_2010@hotmail.com.ar>
2025-11-13 03:09:24 -0300

    Boss animaciones: Daño y muerte
    Falta agregar menu de derrota

--- Assets/Scripts/Enemies/Boss/BossHealth.cs
+++ Assets/Scripts/Enemies/Boss/BossHealth.cs
@@ -1,21 +1,45 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.SceneManagement;
 public class BossHealth : MonoBehaviour
 {
     public int lives;
+    private bool _isDead = false;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public void Damager(int damage)
     {
+        if (_isDead) return; // evita seguir recibiendo daño si ya está muerto
+
         lives -= damage;
+        _animator.SetBool("TakeDamage", true);
 
         if (lives <= 0)
         {
             Die();
         }
+        else
+        {
+            StartCoroutine(ResetTakeDamage());
+        }
     }
-
-    public void Die()
+    private IEnumerator ResetTakeDamage()
     {
-        gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.35f);
+        _animator.SetBool("TakeDamage", false);
+    }
+    private void Die()
+    {
+        _isDead = true;
+
+        _animator.SetBool("IsDead", true);
+
+        Destroy(gameObject, 1.2f);
     }
 }

0c24ee74b05d689702877aeb4c26bd7bb4bdba48
Gabriel Antos <julid_2010@hotmail.com.ar>
2025-11-19 20:14:47 -0300

    Borramos mapa que no se utiliza

--- Assets/Scripts/Ui/UIKeys.cs
+++ Assets/Scripts/Ui/UIKeys.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using System.Collections;
 public class UIKeys : MonoBehaviour
 {
     public PlayerInventory playerinventory;
@@ -8,7 +9,27 @@ public class UIKeys : MonoBehaviour
     public TextMeshProUGUI keysValue;
     // Update is called once per frame
     void Update()
-    {
-        keysValue.text = playerinventory.keys.ToString();
+    {   
+        if (playerinventory.keys <= 4)
+        {
+            keysValue.text = playerinventory.keys.ToString();
+        }
+
+        if(playerinventory.keys >= 4)
+        {
+       
+            StartCoroutine(CleanRoutine());
+        }
+
+
     }
+    private IEnumerator CleanRoutine()
+    {
+        yield return new WaitForSeconds(6f);
+
+        keysValue.text = 0.ToString();
+    }
+
 }
+
+
